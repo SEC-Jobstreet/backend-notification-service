eeabc04f010cb6975a43fe46846aaa8b4a6da443
18120528 <57126456+18120528@users.noreply.github.com>
2024-04-03 12:09:02 +0700

    delete note

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
-//
-const path = require('path'); // Corrected require syntax
+//import
+const path = require('path');
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const mongoose = require('mongoose');
@@ -8,20 +8,20 @@ const uri = require('./models/mongoProfile');
 const addAlert=require('./models/methods/addAlert');
 const addDailyPost=require('./models/methods/addDailyPost');
 const sendAlert=require('./models/methods/sendAlert');
-//
+//set up
 mongoose.connect(uri);
 const packageLoader = protoLoader.loadSync(path.join(__dirname, '/protos/notify.proto'));
 const grpcObject = grpc.loadPackageDefinition(packageLoader);
 const notifyPackage = grpcObject.notifyPackage;
-//
+
 const PORT=40000;
-//
+
 const server = new grpc.Server();
 server.bindAsync(`0.0.0.0:${process.env.PORT||PORT}`, grpc.ServerCredentials.createInsecure(), (err) => {
     if (err) {
         console.error('Server binding failed:', err);
     } else {
-        console.log('Server bound on port 40000'); // Log successful binding
+        console.log(`Server bound on port ${process.env.PORT||PORT}`);
         server.addService(notifyPackage.notify.service, {
             "createAlert": createAlert,
             "createPost": createPost,
@@ -30,8 +30,8 @@ server.bindAsync(`0.0.0.0:${process.env.PORT||PORT}`, grpc.ServerCredentials.cre
         console.log('Server started successfully');
     }
 });
-//
 //functions
+//upload alert
 function createAlert(call, callback) {
     let notifyItem={
         "keyword": call.request.keyword,
@@ -39,10 +39,10 @@ function createAlert(call, callback) {
         "radius": call.request.radius,
         "userName": call.request.userName
     }
-    addAlert(notifyItem);//model.create
-    callback(null, {"ack": true});//response and request must be like proto if not=> no match field
+    addAlert(notifyItem);
+    callback(null, {"ack": true});
 }
-//
+//upload new post
 function createPost(call, callback) {
     let postItem={
         "jobName": call.request.jobName,
@@ -50,9 +50,9 @@ function createPost(call, callback) {
         "location": call.request.location
     }
     addDailyPost(postItem);
-    callback(null, {"ack": true});//response and request must be like proto if not=> no match field
+    callback(null, {"ack": true});
 }
-
+//get matching new posts
 async function getNotifies(call, callback) {
     var postList=await sendAlert(call.request.userName);
     if(postList.length>0)
